c55950a06b3577af46ddf9f70aeb1361555229c8
Kevin Quach <kevinquach2@yahoo.com>
2022-01-17 22:53:53 -0600

    Added a button that clears the board and creates new grid of a size specified by the user

--- js-file.js
+++ js-file.js
@@ -1,21 +1,39 @@
 const container = document.querySelector('div');
-container.style.width = '800px';
-container.style.height = '800px';
-for (let i = 0; i<16; i++) {
-    const newContainer = document.createElement('div');
-    for (let i = 0; i<16; i++) {
-        const newDiv = document.createElement('div');
-        newContainer.appendChild(newDiv);
-        newDiv.style.width = '50px';
-        newDiv.style.height = '50px';
-    }
-    container.appendChild(newContainer);
-}
+container.style.width = '750px';
+container.style.height = '750px';
+let squareDivs = [];
+let squareDivContainers = [];
 
-const squareDivs = document.querySelectorAll('#container div div');
-//squareDivs.forEach(squareDiv => squareDiv.style['background-color'] = 'green');
-squareDivs.forEach(squareDiv => squareDiv.addEventListener('mouseover', changeColor));
+function createGrid(size=16) {
+    for (let i = 0; i<size; i++) {
+        const newContainer = document.createElement('div');
+        newContainer.id = 'squareDivContainer';
+
+        for (let i = 0; i<size; i++) {
+            const newDiv = document.createElement('div');
+            newContainer.appendChild(newDiv);
+            newDiv.style.width = (parseInt(container.style.width.substring(0,container.style.width.indexOf('p'))) / size).toString() + 'px';
+            newDiv.style.height = (parseInt(container.style.height.substring(0,container.style.height.indexOf('p'))) / size).toString() + 'px';
+        }
+        container.appendChild(newContainer);
+    }
+    squareDivs = document.querySelectorAll('#container div div');
+    squareDivContainers = document.querySelectorAll('#squareDivContainer');
+    squareDivs.forEach(squareDiv => squareDiv.addEventListener('mouseover', changeColor));
+}
 function changeColor(e) {
-    console.log(e);
     this.style['background-color'] = 'red';
 }
+
+function clearGrid(e) {
+    squareDivContainers.forEach(squareDivContainer => container.removeChild(squareDivContainer));
+    let newSize = parseInt(prompt("Please enter a new size for your grid"));
+    while(newSize > 100){
+        newSize = parseInt(prompt("Please enter a smaller size"));
+    }
+    createGrid(newSize);
+}
+createGrid();
+
+const button = document.querySelector('button');
+button.addEventListener('click',clearGrid);
